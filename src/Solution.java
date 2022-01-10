505ac70198fff4f14d76180f30c2c3fd70763e2e
Ayeon-Jeong <jdh961224@gmail.com>
2022-01-10 16:49:23 +0900

    stream study

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Solution {
 
     public static void main(String[] args) {
 
 
+        List<String> list = Arrays.asList("a", "b", "c");
+        Stream<String> listStream = list.stream();
+
         
+
+
+
+
         System.out.println("Hello");
     }
 }
