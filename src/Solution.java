77824f8aff260165a2a2f7194b4960b974a291e7
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2022-01-12 23:23:52 +0900

    완주하지못한선수

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,40 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Solution {
 
+    public String solution(String[] participant, String[] completion){
+        String answer = "";
+
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String player : participant){
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        for(String player : completion){
+            map.put(player, map.get(player) - 1);
+        }
+
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+
+        while(iter.hasNext()){
+            Map.Entry<String, Integer> entry = iter.next();
+            if(entry.getValue() != 0){
+                answer = entry.getKey();
+                break;
+            }
+        }
+
+        return answer;
+    }
+
+
+
     public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
 
+        System.out.println(new Solution().solution(participant, completion));
 
-        List<String> list = Arrays.asList("a", "b", "c");
-        Stream<String> listStream = list.stream();
-
-        
-
-
-
-
-        System.out.println("Hello");
+//        System.out.println("Hello");
     }
 }
