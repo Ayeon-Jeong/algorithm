7cd7b3728cc1bc56625cc7917bc807a62e95fa28
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2022-01-17 01:06:46 +0900

    0117

--- src/Solution2.java
+++ src/Solution2.java
@@ -15,35 +15,28 @@ public class Solution2 {
         Map map = new HashMap();
 
         int i = 0;
-        for(String p: phone){
+        for(String p: phone) {
             map.put(p, i++);
 
         }
 
-
-        System.out.println(map.size());
-
-        System.out.println("119".hashCode());
-        System.out.println("97674223".hashCode());
-        System.out.println("1195524421".hashCode());
-        System.out.println("1".hashCode());
-
-
-
         Iterator<String> keys = map.keySet().iterator();
 
-        while( keys.hasNext() ){
-            String key = keys.next();
-            System.out.println( String.format("키 : %s, 값 : %d", key, map.get(key)) );
-        }
-
-
-//        for(String p: phone){
-//
-//            for(int i = 1; i < p.length(); i++){
-//
+//        while( keys.hasNext() ){
+//            String key = keys.next();
+//            for(int j = 1; j < key.length(); j++) {
+//                String tmp = key.substring(0, j);
+//                if (map.containsKey(tmp)) {
+//                    System.out.println("false");
+//                }
 //            }
 //        }
+
+        System.out.println(map.keySet().iterator().hasNext());
+        System.out.println(map.keySet().iterator().next());
+
+
+
 
     }
 }
