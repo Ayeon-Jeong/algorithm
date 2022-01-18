374fbc7facfc288dc43688e93842a248f1fbb1f3
Ayeon-Jeong <57283339+Ayeon-Jeong@users.noreply.github.com>
2022-01-19 00:00:37 +0900

    0119

--- src/HashMap/maptest.java
+++ src/HashMap/maptest.java
@@ -2,9 +2,16 @@ package HashMap;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class maptest {
+
+    int index;
+    int count;
+
+    public maptest(int index, int count) {
+        this.index = index;
+        this.count = count;
+    }
 
     public static void main(String[] args) {
 
@@ -15,27 +22,37 @@ public class maptest {
         for(int i = 0; i < genres.length; i++){
             genre.put(genres[i], genre.getOrDefault(genres[i], 0) + plays[i]);
         }
+//        System.out.println(genre);
 
         List<Map.Entry<String, Integer>> entries = genre.entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
                 .collect(Collectors.toList());
 
-        Stream<Map.Entry<String, Integer>> sorted = genre.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()));
-
-        Map<String, List<Integer>> list = new HashMap<>();
+        Map<String, List<maptest>> list = new HashMap<>();
 
         for(String s: genre.keySet()){
-            list.put(s, new ArrayList<>());
+            list.put(s, new ArrayList<maptest>());
         }
 
-        list.get("pop").add(3);
-        list.get("classic").add(5);
-        System.out.println(list);
+        for(int i = 0; i < genres.length; i++){
+            list.get(genres[i]).add(new maptest(i, plays[i]));
+        }
 
-        /
+//        list.get("pop").add(new maptest(0, 500));
 
-//        System.out.println(sorted.toList().get(0));
+
+        System.out.println(list.get("pop").stream().count());
+
+        List<Integer> answer = new ArrayList<>();
+
+        for(Map.Entry<String, Integer> g: entries){
+            int cnt = 0;
+            while(cnt < 2){
+                //answer.add(노래번호)
+                //노래번호 = list.get("장르").get(0).index
+                answer.add(list.get())
+            }
+        }
 
     }
 }
